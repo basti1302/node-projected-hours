dbb3eee1ef5cd8977f78e3b09f40b3267605a8af
Bastian Krol <bastian.krol@web.de>
2015-04-21 21:45:51 +0200

    progress

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict';
 
-function Calculation(hoursWorked, hoursPerWeek, daysPerWeek) {
+var moment = require('moment');
+var workwork = require('workwork');
+
+function Calculation(hoursWorked, hoursPerWeek) {
   this.hoursWorked = hoursWorked || 0;
   this.hoursPerWeek = hoursPerWeek || 40;
-  this.daysPerWeek = daysPerWeek || 5;
+  // Can't make this easily configurable because workwork and liberty both
+  // assume Monday to Friday to be working days and Saturday and Sunday to be
+  // non-working days
+  this.daysPerWeek = 5;
+  this.regions = null;
 }
 
 Calculation.prototype.getHoursWorked = function() {
@@ -22,20 +29,53 @@ Calculation.prototype.setHoursPerWeek = function(hoursPerWeek) {
   this.hoursPerWeek = hoursPerWeek;
 };
 
-Calculation.prototype.getDaysPerWeek = function() {
-  return this.daysPerWeek;
-};
-
-Calculation.prototype.setDaysPerWeek = function(daysPerWeek) {
-  this.daysPerWeek = daysPerWeek;
-};
-
 Calculation.prototype.getHoursPerDay = function() {
   return this.hoursPerWeek / this.daysPerWeek;
 };
 
 Calculation.prototype.getDaysWorked = function() {
   return this.hoursWorked / this.getHoursPerDay();
+};
+
+Calculation.prototype.setRegions = function(regions) {
+  this.regions = regions;
+};
+
+Calculation.prototype.getWorkingDays = function(from, until) {
+  if (!this.regions) {
+    throw new Error('This calculation requires that you set one or several ' +
+        'regions with setRegions() before.');
+  }
+  return workwork(this.regions).between(from, until).length;
+};
+
+Calculation.prototype.getWorkingDaysUntil = function(until) {
+  var startOfYear = moment(until).startOf('year');
+  return this.getWorkingDays(startOfYear, until);
+};
+
+Calculation.prototype.getWorkingHours = function(from, until) {
+  return this.getWorkingDays(from, until) * this.getHoursPerDay();
+};
+
+Calculation.prototype.getWorkingHoursUntil = function(until) {
+  return this.getWorkingDaysUntil(until) * this.getHoursPerDay();
+};
+
+Calculation.prototype.getWorkingHoursYearTotal = function(year) {
+  year = year || moment().year(); // default: current year
+  var startOfYear = moment({ years: year }).startOf('year');
+  var endOfYear = moment({ years: year }).endOf('year');
+  return this.getWorkingHours(startOfYear, endOfYear);
+};
+
+/*
+ * working hours from start of year until given date divided by working hours in
+ * the whole year (the year is always taken from the given date.
+ */
+Calculation.prototype.getWorkingHoursFraction = function(until) {
+  var year = moment(until).year();
+  return this.getWorkingHoursUntil(until) / this.getWorkingHoursYearTotal(year);
 };
 
 module.exports = Calculation;
--- test/index.js
+++ test/index.js
@@ -1,64 +1,125 @@
 'use strict';
 
 var test = require('tape');
+var moment = require('moment');
 var Calculation = require('..');
 
-var calc;
-
-function init() {
-  calc = new Calculation();
-}
-
 test('default values', function (t) {
-  init();
+  var calc = new Calculation();
   t.equal(calc.getHoursWorked(), 0);
   t.equal(calc.getHoursPerWeek(), 40);
-  t.equal(calc.getDaysPerWeek(), 5);
+  t.equal(calc.daysPerWeek, 5);
   t.equal(calc.getHoursPerDay(), 8);
   t.end();
 });
 
 test('non default values', function (t) {
-  init();
+  var calc = new Calculation();
   calc.setHoursWorked(200);
   calc.setHoursPerWeek(42);
-  calc.setDaysPerWeek(6);
   t.equal(calc.getHoursWorked(), 200);
   t.equal(calc.getHoursPerWeek(), 42);
-  t.equal(calc.getDaysPerWeek(), 6);
   t.end();
 });
 
 test('configure by constructor', function (t) {
-  init();
-  calc = new Calculation(42, 34, 4);
+  var calc = new Calculation(42, 34);
   t.equal(calc.getHoursWorked(), 42);
   t.equal(calc.getHoursPerWeek(), 34);
-  t.equal(calc.getDaysPerWeek(), 4);
   t.end();
 });
 
 test('hours per day', function(t) {
-  calc = new Calculation(42, 34, 4);
-  t.equal(calc.getHoursPerDay(), 8.5);
+  var calc = new Calculation();
+  calc.setHoursPerWeek(42);
+  t.equal(calc.getHoursPerDay(), 8.4);
   t.end();
 });
 
 test('hours per day/default values', function (t) {
-  init();
+  var calc = new Calculation();
   t.equal(calc.getHoursPerDay(), 8);
   t.end();
 });
 
+test('days worked/default values', function (t) {
+  var calc = new Calculation();
+  t.equal(calc.getDaysWorked(), 0);
+  t.end();
+});
+
 test('days worked', function(t) {
-  init();
+  var calc = new Calculation();
   calc.setHoursWorked(204);
   t.equal(calc.getDaysWorked(), 25.5);
   t.end();
 });
 
-test('days worked/default values', function (t) {
-  init();
-  t.equal(calc.getDaysWorked(), 0);
+test('days worked non-default hours per day', function(t) {
+  var calc = new Calculation();
+  calc.setHoursPerWeek(45);
+  calc.setHoursWorked(180);
+  t.equal(calc.getDaysWorked(), 20);
+  t.end();
+});
+
+test('working days without regions throws error', function(t) {
+  var calc = new Calculation();
+  t.throws(function() { calc.getWorkingDays(moment(), moment()); }, /Error/);
+  t.end();
+});
+
+test('working days from date to date', function(t) {
+  var calc = new Calculation();
+  calc.setRegions(['de', 'de_nw']);
+  t.equal(calc.getWorkingDays('2015-01-01', '2015-12-31'), 253);
+  t.end();
+});
+
+test('working days until without regions throws error', function(t) {
+  var calc = new Calculation();
+  t.throws(function() { calc.getWorkingDaysUntil(moment()); }, /Error/);
+  t.end();
+});
+
+test('working days until date (from start of year)', function(t) {
+  var calc = new Calculation();
+  calc.setRegions(['de', 'de_nw']);
+  t.equal(calc.getWorkingDaysUntil('2014-06-30'), 122);
+  t.end();
+});
+
+test('working hours without regions throws error', function(t) {
+  var calc = new Calculation();
+  t.throws(function() { calc.getWorkingHours(moment(), moment()); }, /Error/);
+  t.end();
+});
+
+test('working hours from date to date', function(t) {
+  var calc = new Calculation();
+  calc.setRegions(['de', 'de_nw']);
+  t.equal(calc.getWorkingHours('2015-01-01', '2015-12-31'), 2024);
+  t.end();
+});
+
+test('working hours until without regions throws error', function(t) {
+  var calc = new Calculation();
+  t.throws(function() { calc.getWorkingHoursUntil(moment()); }, /Error/);
+  t.end();
+});
+
+test('working hours until date (from start of year)', function(t) {
+  var calc = new Calculation();
+  calc.setRegions(['de', 'de_nw']);
+  t.equal(calc.getWorkingHoursUntil('2014-06-30'), 976);
+  t.end();
+});
+
+test('working hours fraction', function(t) {
+  var calc = new Calculation();
+  calc.setRegions(['de', 'de_nw']);
+  var fraction = calc.getWorkingHoursFraction('2015-06-30');
+  t.ok(fraction > 0.482);
+  t.ok(fraction < 0.483);
   t.end();
 });
